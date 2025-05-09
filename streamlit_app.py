516fdc8ef27dac5049575ccaf80f1275a48a1d2e
Parksiurn <112524812+Parksiurn@users.noreply.github.com>
2025-05-09 09:20:31 +0000

    대문 페이지 자기소개로 바꾸기

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("소개")
+st.info(
+    "안녕하세요! 반갑습니다. 저는 입니다"
 )
+# 페이지 구조용 제목 출력
+st.title("성")
+st.header("유")
+st.subheader("진")
+# st.sidebar: 사이드바 영역에 콘텐츠를 배치합니다
+st.sidebar.title("📌 사이드바 메뉴")
+option = st.sidebar.selectbox("옵션을 선택하세요", ["성유진", "박선준", "꺼임"])
+st.write("선택한 옵션:", option)
